9524a946149eb962e7a5cf937a1808f8c430e2d2
Miriam Ahlberg <miriam.ahlberg@gmail.com>
2018-11-03 13:46:03 +0100

    Miriams solution with ajax popup

---
+++ miriams_solution/modal.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+
+	$('.simple-ajax-popup-align-top').magnificPopup({
+            type: 'ajax',
+            alignTop: true,
+            overflowY: 'scroll' // as we know that popup content is tall we set scroll overflow by default to avoid jump
+	});
+	
+});
