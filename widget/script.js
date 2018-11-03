afc76777c9c2ef3ddbdf21b41c80155c5ec060b6
Felixfranzen <felixfranzn@gmail.com>
2018-11-03 16:21:57 +0100

    Add ability to toggle form visibility.

--- widget/script.js
+++ widget/script.js
@@ -1,18 +1,32 @@
 const formTemplate = `
-<form class="form-container">
-  <select class="tag-selector">
-    <option selected value="">Välj</option>
-    <option value="Physical violence">Physical violence</option>
-    <option value="Verbal abuse">Verbal abuse</option>
-  </select>
-  <div class="tags">
+<div class="content-wrapper">
+  <form class="form-container">
+    <button type="button" class="close">X</button>
+    <label>
+    <span>Tags</span>
+    <select class="tag-selector">
+      <option selected value="">Choose tags</option>
+      <option value="Physical violence">Physical violence</option>
+      <option value="Verbal abuse">Verbal abuse</option>
+    </select>
+    </label>
+    <div class="tags">
 
+    </div>
+    <label>
+      <span>Incident description</span>
+      <textarea name="" id="" cols="30" rows="10" placeholder="What happened?" class="message"></textarea>
+    </label>
+    <label>
+      <span>Alias</span>
+      <input type="text" placholder="alias" class="alias"/>
+    </label>
+    <button type="submit">Send report</button>
+  </form>
+  <div class="toggle-wrapper">
+    <button class="hand-toggle" type="submit"><span>OPEN</span></button>
   </div>
-  <textarea name="" id="" cols="30" rows="10" placeholder="What happened?" class="message"></textarea>
-  <input type="text" placholder="alias" class="alias"/>
-  <button type="submit">Send report</button>
-</form>
-
+</div>
 `
 // state ------------------------------
 const selectedTags = [];
@@ -21,19 +35,27 @@ const selectedTags = [];
 const containerEl = document.getElementById('helping-hand');
 containerEl.innerHTML = formTemplate;
 
+const handButtonEl = containerEl.querySelector('.hand-toggle');
 const tagSelectorEl = containerEl.querySelector('.tag-selector');
 const tagListEl = containerEl.querySelector('.tags');
 const messageInputEl = containerEl.querySelector('.message');
 const aliasInputEl = containerEl.querySelector('.alias');
 const formEl = containerEl.querySelector('.form-container');
+const closeButton = containerEl.querySelector('.close');
+
 
 // helpers ------------------------------
 const renderTags = (tags) => {
   return `<ul>${tags.map(t => `<li>${t}</li>`).join('')}</ul>`
 }
 
+const toggleFormVisibility = (show) => {
+  const prop = show ? 'flex' : 'none';
+  formEl.style.display = prop;
+}
+
 // listeners ------------------------------
-tagSelectorEl.addEventListener('change', (e) => {
+tagSelectorEl.addEventListener('change', event => {
   const value = e.currentTarget.value;
   if (value.length > 0 && !selectedTags.includes(value)){
     selectedTags.push(value);
@@ -41,7 +63,7 @@ tagSelectorEl.addEventListener('change', (e) => {
   }
 });
 
-formEl.addEventListener('submit', (event) => {
+formEl.addEventListener('submit', event => {
   event.preventDefault();
   const message = messageInputEl.value;
   const alias = aliasInputEl.value;
@@ -54,5 +76,13 @@ formEl.addEventListener('submit', (event) => {
 
   console.log(formData);
   // todo post to api!
-
 })
+
+closeButton.addEventListener('click', event => {
+  toggleFormVisibility(false);
+})
+
+handButtonEl.addEventListener('click', event => {
+  const visible = formEl.style.display !== 'none';
+  toggleFormVisibility(!visible);
+});
